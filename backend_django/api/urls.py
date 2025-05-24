d501501f91d39cb39a53ed8ecafdd1c7824b8b23
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2025-05-24 13:45:45 +0200

    week5: create unified deck_handler for GET/POST methods

--- backend_django/api/urls.py
+++ backend_django/api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('deck/', views.create_deck, name='create_deck'),  # POST
-    path('deck/',views.list_decks, name='list_decks'),  # GET
+    path('deck/', views.deck_handler, name='deck_handler'),
 ]
